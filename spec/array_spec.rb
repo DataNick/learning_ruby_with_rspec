0d022f67c25835e87b2261a8cde9354a366522dd
DataNick <DataNick@users.noreply.github.com>
2015-11-11 17:51:34 -0500

    Passing test for adding two arrays

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,4 +2,13 @@ require 'spec_helper'
 
 describe Array do
 
+  describe "Adding two array objects together" do
+    it "returns a new array with concatenated results" do
+      array_one = [1, 2]
+      array_two = [5, 6]
+      concat = array_one + array_two
+      expect(concat).to eq([1, 2, 5, 6])
+    end
+  end
+
 end
