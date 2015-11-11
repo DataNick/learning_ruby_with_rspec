69342ac4173a779b40013735e120463a4fba9204
DataNick <DataNick@users.noreply.github.com>
2015-11-11 02:12:57 -0500

    Passing test for #all?

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
+# Enumerable mixin provides collection classes with traversal and searching methods, plus ability to sort.
 
 describe Enumerable do
+
+  describe "#all?{|obj|block}" do
+    it "passes each element of the collection to the block; returns true if block never returns false or nil" do
+      array = %w(entourage mad_men true_detective).all?{|obj| obj.length >= 7}
+      expect(array).to eq(true)
+    end
+  end
 
 end
