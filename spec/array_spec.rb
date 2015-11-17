37aef66e83167e115f7d90cb30465ee9f6684d7a
DataNick <DataNick@users.noreply.github.com>
2015-11-16 20:31:32 -0500

    Passing test for #new

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -11,4 +11,16 @@ describe Array do
     end
   end
 
+  describe "#new" do
+    it "returns a new array" do
+      array = Array.new
+      expect(array).to eq([])
+    end
+
+    it "returns a new array of 3 elements named 'object'" do
+      array = Array.new(3, "object")
+      expect(array).to eq(["object", "object", "object"])
+    end
+  end
+
 end
