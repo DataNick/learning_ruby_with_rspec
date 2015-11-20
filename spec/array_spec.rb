f09346b017fa47b05ccfa7caf66a69c61409a69e
DataNick <DataNick@users.noreply.github.com>
2015-11-19 22:33:20 -0500

    Passing test for #product(other_ary)

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -809,4 +809,14 @@ describe Array do
     end
   end
 
+  describe "#product(other_ary) > new_ary" do
+    it "returns new array of all combinations of self and other_array" do
+      a = [1,2]
+      b = %w(a b)
+      c = [true, false]
+      result = a.product(b,c)
+      expect(result).to eq([[1,"a",true], [1,"a",false],[1,"b",true],[1,"b",false],[2,"a",true],[2,"a",false],[2,"b",true],[2,"b",false]])
+    end
+  end
+
 end
