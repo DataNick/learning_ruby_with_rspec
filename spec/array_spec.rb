a95665dfd7bb972623ebb98f143ee5b01fc1a224
DataNick <DataNick@users.noreply.github.com>
2015-11-20 11:12:46 -0500

    Passing test for #sort_by

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -958,5 +958,13 @@ describe Array do
     end
   end
 
+  describe "#sort_by!{|obj|block} > array" do
+    it "takes a block with only one parameter" do
+      a = [1,2,3,4,5,6,7,8,9,0]
+      a.sort_by!{|x| -x}
+      expect(a).to eq([9,8,7,6,5,4,3,2,1,0])
+    end
+  end
+
 
 end
