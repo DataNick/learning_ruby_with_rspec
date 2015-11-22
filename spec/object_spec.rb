69907b22d8000f98ff3ba74519fb44af7ae75fc2
DataNick <DataNick@users.noreply.github.com>
2015-11-22 03:54:08 -0500

    Passing test for #nil?

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+
+describe Object do
+
+  describe "#nil?" do
+    it "returns true if object == nil" do
+      result = nil.nil?
+      expect(result).to eq(true)
+    end
+  end
+
+
+end
