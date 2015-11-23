493e1f4550bc1c1c0419fc322f2f4628dffb2ed2
DataNick <DataNick@users.noreply.github.com>
2015-11-23 17:01:18 -0500

    Passing test for #method

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -9,5 +9,13 @@ describe Object do
     end
   end
 
+  describe "#method(sym)" do
+    it "looks up a named method as a receiver to obj and returns a method object" do
+      a = [3,5,1,4,2].method(:sort) #will return <Method: Array#sort!>
+      b = a.call
+      expect(b).to eq([1,2,3,4,5])
+    end
+  end
+
 
 end
