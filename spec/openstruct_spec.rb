cd10df9a82f154fa97b3dbff1c8779876eec25a5
DataNick <DataNick@users.noreply.github.com>
2016-02-12 16:07:19 -0500

    Passing test for OpenStruct.new

--- spec/openstruct_spec.rb
+++ spec/openstruct_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 require 'ostruct'
 
+# Data structure similar to a hash. Defines arbitrary attributes and with their accompanying values
+# Makes use of Ruby's metaprogramming to define mehtods on the class itself
+
 describe OpenStruct do
+
+  describe "making a new OpenStruct object" do
+    it "takes a hash object as an argument" do
+      hash = {name: "Harry", age: 45, country: "USA"}
+      data = OpenStruct.new(hash)
+      expect(data[:name]).to eq("Harry")
+    end
+  end
 
   describe "#[]name" do
     it "returns value of a member" do
