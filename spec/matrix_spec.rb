d16f93a40fff779bbc0197932690dad727a94d70
DataNick <DataNick@users.noreply.github.com>
2016-02-17 07:38:32 -0500

    Passing test for #[]

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 
 describe Matrix do
 
+  describe "#[]" do
+    it "creates a matrix where each argument is a row" do
+      matrix = Matrix[ [ 2, 2 ],[ 5, 5 ] ]
+      expect(matrix).to eq(Matrix[ [ 2, 2 ],[ 5, 5 ] ])
+    end
+  end
+
   describe "#diagonal" do
     it "returns a matrix with specific numbers forming a diagonal line" do
       diagonal =  Matrix.diagonal(1,2,3,4)
